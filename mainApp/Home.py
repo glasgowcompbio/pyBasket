fcabe0eb2dcdbfcae1fd261daff9e91e9dbdeb80
Marina Flores Payan <s1801634@ed.ac.uk>
2023-07-31 19:28:47 +0100

    New text, small fixes

--- mainApp/Home.py
+++ mainApp/Home.py
@@ -6,6 +6,16 @@ from streamlit_option_menu import option_menu
 from explorer import Data, readPickle
 from analysis import Analysis
 
+def openWikipedia(drug):
+    webpage_link = "https://en.wikipedia.org/wiki/"+drug
+    webbrowser.open(webpage_link)
+def openPubMed(drug):
+    webpage_link = "https://pubmed.ncbi.nlm.nih.gov/?term={}" + drug
+    webbrowser.open(webpage_link)
+def openDrugBank(num):
+    webpage_link = "https://go.drugbank.com/drugs/" + num
+    webbrowser.open(webpage_link)
+
 st.set_page_config(
     page_title="pyBasket",
     page_icon="bi bi-basket",
@@ -15,22 +25,23 @@ st.set_page_config(
 add_logo()
 
 sideBar()
-st.header("pyBasket")
+st.header("Home")
 st.write("---")
-menu = option_menu(None, ["Overview", "Data Upload", "File information", 'Drug information'],
-    icons=["bi bi-basket", 'cloud-upload', "list-task", 'capsule'],
+menu = option_menu(None, ["Overview", "Data Upload", "Clinical trial information"],
+    icons=["bi bi-basket", 'cloud-upload', 'capsule'],
     menu_icon="cast", default_index=0, orientation="horizontal")
 
+#Overview subpage: general information about the app and basic steps to use it
 if menu == "Overview":
-    st.write("### Welcome 👋")
+    st.subheader("Welcome 👋")
     st.write(" ")
     st.write("The pyBasket App is a user-friendly interactive platform that allows the visualisation and exploration"
              " of results obtained from the pyBasket pipeline. The main goal of this App is to aid in the investigation of"
-             " the omics profiles of the samples or patients involved in the clinical trials and gain potential biological insights"
-             " into response patterns to a drug or treatment showed by the different subgroups.")
+             " the omics profiles of the samples or patients involved in the clinical trials and gain biological insights"
+             " into the response patterns showed by the different subgroups to a drug.")
     st.write(" ")
     st.write("##### Basket trials")
-    st.write("Basket trials are a type of early phase clinical trial that tests how well a new drug or treatment works in"
+    st.write("Basket trials are a type of early phase clinical trial design that tests how well a new drug or treatment works in"
              " groups of patients that have different types of cancer but share the same molecular or genomic mutation or biomarker."
              " This innovative approach is more effective and cost-efficient, it requires fewer patients for the study and allows "
              "intractable or rare cancer patients to be also included in general clinical trials."
@@ -38,44 +49,47 @@ if menu == "Overview":
              "subpopulations of patients and allows for a more personalised treatment approach.")
     st.write(" ")
     st.write("##### pyBasket")
+    url = "https://glasgowcompbio.github.io/pyBasket/"
     st.write("pyBasket is a two-stage approach that incorporates omics data into basket trial response prediction from cancer patients."
              " In the first stage, patients are clustered based on their omics profile using K-means clustering. "
-             " These clusters assignments are used within a Hierarchical Bayesian model along with basket response rates to "
-             " estimate overall response rates and predict interaction terms between baskets and clusters.")
-
+             " These clusters assignments are used within a hierarchical Bayesian model along with basket response rates to "
+             " estimate overall response rates and predict interaction terms between baskets and clusters. Further information about the pyBasket model can be found here: [link](%s)" % url)
     st.write("---")
     with st.expander("##### Basic steps to use this APP"):
-        #st.write("##### General steps ")
         st.write(" ")
         col11, col12 = st.columns((2,2))
         with col11:
             st.write("##### :one: Upload the data")
-            st.write("Navigate to the _Data Upload_ tab in this same page. Select a pickle file with results obtained from the pyBasket pipeline.")
+            st.write("Navigate to the _Data Upload_ tab in this same page. Select a pickle file with results obtained from the pyBasket pipeline to be uploaded.")
             st.write(" ")
             st.write("##### :two: Check file information")
             st.write(
-                "Navigate to the _File Information_ tab in this same page where you can find information about the file uploaded.")
+                "Navigate to the _File Information_ tab in this same page to find information about the file uploaded.")
             st.write(" ")
             st.write("##### :three: Check drug information")
             st.write(
-                "Navigate to the _Drug Information_ tab in this same page where you can find further information about the drug tested in the basket trial being analysed.")
+                "Navigate to the _Drug Information_ tab in this same page to find further information about the drug tested in the basket trial being analysed.")
             st.write(" ")
         with col12:
             st.write("##### :four: Explore the data")
             st.write(
-                "Navigate to the _Data Exploration_ subpage located in the left sidebar. There you can find general information about the samples and features analysed.")
+                "Navigate to the _Data Exploration_ subpage located in the left sidebar. Find more general information about the samples, the predicted treatment response rates and "
+                "perform Dimensionality reduction and Differential Expression Analysis.")
             st.write(" ")
             st.write("##### :five: Select and analyse a basket*cluster interaction")
             st.write("In the left sidebar, select a cluster number and a basket/tissue. Samples that fall in this interaction will be selected. "
-                     "Navigate to the _Interactions_ subpage located in the left sidebar to further explore results from the samples included in the selected basket*cluster interaction")
+                     "Navigate to the _Basket-Cluster_Interactions_ subpage located in the left sidebar to further explore results from the samples included in the selected basket-cluster interaction")
             st.write(" ")
             st.write("##### :six: Use interpretable ML methods ")
             st.write(
-                "Navigate to the _Feature importance_ subpage located in the left sidebar to use interpretable Machine Learning methods and explore important features.")
+                "Navigate to the _Feature Importance_Analysis subpage located in the left sidebar to use interpretable Machine Learning methods and explore important features.")
             st.write(" ")
 
+#Upload data subpage
 if menu == "Data Upload":
-    st.markdown("#### Please upload your data")
+    st.subheader("Please upload your data")
+    files = "https://gla-my.sharepoint.com/personal/ronan_daly_glasgow_ac_uk/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fronan%5Fdaly%5Fglasgow%5Fac%5Fuk%2FDocuments%2FJoe%20Files%2FpyBasket&ga=1"
+    st.write("Click the box to select a pickle file with results from the pyBasket pipeline to be uploaded. Other results files can be sourced from the pyBasket project's [OneDrive folder](%files).")
     input_file = st.file_uploader('Upload your data file (.py format)', type='p')
     file_name = ""
     if input_file is not None:
@@ -98,33 +112,14 @@ if menu == "Data Upload":
         st.success('Analysis ready', icon="✅")
         st.info("Current file: {}".format(st.session_state['File Name']))
 
-def openDrugBank(num):
-    webpage_link = "https://go.drugbank.com/drugs/" + num
-    webbrowser.open(webpage_link)
-
-def openGoogleScholar(drug):
-    webpage_link = "https://scholar.google.com/scholar?hl=es&as_sdt=0%2C5&q={}&btnG=".format(drug)
-    webbrowser.open(webpage_link)
-
-def openWikipedia(drug):
-    webpage_link = "https://en.wikipedia.org/wiki/"+drug
-    webbrowser.open(webpage_link)
-
-def openPubMed(drug):
-    webpage_link = "https://pubmed.ncbi.nlm.nih.gov/?term={}" + drug
-    webbrowser.open(webpage_link)
-
-if menu == "File information":
+#File and drug information subpage
+if menu == "Clinical trial information":
     st.subheader("File information")
+    st.write("Once a file of results is uploaded, general information about the file is displayed here.")
     if "data" in st.session_state:
         st.session_state["data"].fileInfo()
-    else:
-        st.warning("No data found. Please upload results in the 'Data Upload' tab")
-
-
-if menu == 'Drug information':
-    st.subheader("Drug information")
-    if "data" in st.session_state:
+        st.write(" ")
+        st.subheader("Drug information")
         name = st.session_state["File Name"].split('_')
         drug = name[2]
         st.write("#### The drug used in this basket trial was: **{}**".format(drug))
@@ -139,7 +134,6 @@ if menu == 'Drug information':
                  " pharmacological and pharmaceutical data with information about drug target (sequence, structure, pathway, etc."
                  " Further pharmacological and chemical information about {} can be found in the link to _DrugBank_ below.".format(drug))
         st.button('Open DrugBank', on_click=openDrugBank, args=(accession_num,))
-
         st.write("#### _PubMed_")
         st.write("_PubMed_ is a free full-text archive of biomedical and life sciences print and electronic journal literature,"
                  " and supports current biomedical and healthcare research and practice. Other research and journal literature related to "
--- mainApp/pages/1_Data_Exploration.py
+++ mainApp/pages/1_Data_Exploration.py
@@ -17,19 +17,23 @@ if "data" in st.session_state:
     data = st.session_state["data"]
     heatmap = heatMap(st.session_state["saved data"],st.session_state["File Name"])
     analysis = st.session_state["Analysis"]
+    #Subpage showing information about the samples: Number and AAC response
     if menu == "Samples information":
         st.subheader("Number of samples")
         st.write(
-            "The number of samples can be shown either by cluster or basket/tissue. The number of responsive and non-responsive"
-            " samples within groups can also be explored by selecting 'Group by response'.")
+            "The number of samples can be shown either by cluster or basket/tissue. The number of Responsive and Non-responsive"
+            " samples within groups can also be explored. ")
         st.write(" ")
         col11, col12 = st.columns((1, 1))
         with col11:
+            #Option to select level of grouping: clusters or baskets
             option_tab1 = st.selectbox("Select how to group samples", ('Clusters', 'Baskets/Tissues'),
                                        key="option-tab1")
         with col12:
             st.write(" ")
+            #Option to group samples by response within groups
             RD = st.checkbox("Group by response", key="responsive")
+            #Option to show data in a table format
             RawD = st.checkbox("Show raw data", key="raw-data")
         if option_tab1 == "Clusters":
             if RawD:
@@ -45,7 +49,7 @@ if "data" in st.session_state:
         st.subheader("AAC response")
         st.write(
             "The Area Above the Curve (AAC) is a measure used to analyse drug response and quantify the effect of"
-            " a drug over a period of time. In the context of he GDSC dataset, the AAC is the measure of the cell"
+            " a drug over a period of time. In the context of the GDSC dataset, the AAC is the measure of the cell"
             " or cell line overall survival in response to the drug: the larger the AAC, the more resistance to the"
             " drug is shown."
             " "
@@ -54,28 +58,32 @@ if "data" in st.session_state:
         st.write(" ")
         col21, col22 = st.columns((1, 1))
         with col21:
+            # Option to select level of colouring: none, clusters or baskets
             option_tab2 = st.selectbox("Select how to group samples", ('None', 'Clusters', 'Baskets/Tissues'),
                                        key="option-tab2")
         with col22:
             st.write(" ")
             st.write(" ")
+            #Option to show data in a table format
             RawD_AAC = st.checkbox("Show raw data", key="raw-data-AAC")
         if option_tab2 == 'None':
             data.AAC_scatterplot(RawD_AAC)
         else:
             with col22:
+                # Option to group samples by response within groups
                 RD_AAC = st.checkbox("Group by response", key="responsive-AAC")
             if option_tab2 == "Clusters":
                 data.AAC_response("cluster_number", RD_AAC, "Cluster number", RawD_AAC)
             elif option_tab2 == 'Baskets/Tissues':
                 data.AAC_response("tissues", RD_AAC, "Tissue/Basket", RawD_AAC)
     elif menu == "pyBasket results":
-
+        # Option to select level of grouping: clusters or baskets
         option_page2 = st.selectbox("Select group", ('Clusters', 'Baskets/Tissues'),
                                     key="option-page2")
         st.subheader("Inferred response (mean) probability")
         st.write("In the second stage of the pyBasket pipeline, a Hierarchical Bayesian model is used to estimate the"
                  " overall mean probability of each basket or cluster to be responsive to the treatment.")
+        # Option to show data in a table format
         RawD_prob = st.checkbox("Show raw data", key="raw-data-prob")
         if option_page2 == 'Baskets/Tissues':
             data.barInferredProb("baskets",RawD_prob)
@@ -84,47 +92,54 @@ if "data" in st.session_state:
         st.subheader("Empirical Cumulative Distribution")
         st.write("The Hierarchical Bayesian model returns a single (mean) value rather than the real distribution. The Empirical Cumulative "
                  "Distribution Function (ECDF) provides an empirical approximation of the underlying distribution of the data and assigns "
-                 "probabilities taking into account the number of samples. "
-                 )
+                 "probabilities taking into account the number of samples. ")
         st.write("The ECDF is constructed by arranging the observed data points in ascending order and assigning a cumulative probability to each point. "
                  "The cumulative probability for a sample is calculated as the fraction of data points that are less than or equal to that value."
                  " The credible interval can be chosen below, which is the range containing a particular percentage of probable values.")
-
         col1, col2 = st.columns((2, 2))
         with col2:
+            #User input for credible interval
             cred_inter = st.number_input('Credible interval', value = 90)
             st.caption("90% credible interval shown by default")
         if option_page2 == "Clusters":
             with col1:
+                #Option to select cluster to show ECDF
                 clusterA = st.selectbox("Select cluster", data.clusters_names)
+            #Option to show results in table format
             RawD_ecdf = st.checkbox("Show raw data", key="raw-data-ecdf")
             cluster_choice = data.clusters_names.index(clusterA)
             data.ecdf_indiv("clusters",clusterA,cluster_choice,RawD_ecdf,cred_inter)
         elif option_page2 == 'Baskets/Tissues':
             with col1:
+                # Option to select basket/tissue to show ECDF
                 basketA = st.selectbox("Select basket", data.baskets_names)
+            # Option to show results in table format
             RawD_ecdf = st.checkbox("Show raw data", key="raw-data-ecdf")
             basket_choice = data.baskets_names.index(basketA)
             data.ecdf_indiv("baskets",basketA, basket_choice,RawD_ecdf,cred_inter)
-
     elif menu == "Statistics":
         tab21, tab22, tab23 = st.tabs(["Dimensionality reduction", "Prototypes", "Differential expression"])
+        #Dimensionality reduction subpage (PCA)
         with tab21:
             st.subheader("Dimensionality reduction")
-            st.write("The goal of dimensionality reduction techniques is to project high-dimensionality data to a lower "
+            st.write("The goal of dimensionality reduction is to project high-dimensionality data to a lower "
                      "dimensional subspace while preserving the essence of the data and the maximum amount of information.")
             st.write("Principal Component Analysis (PCA) is a dimensionality reduction method that enables the visualisation"
-                     " of high-dimensional data. The results for PCA on the data can be explored for the data grouped by "
+                     " of high-dimensional data capturing as much as possible of its variation.  "
+                     "The results for PCA on the data can be explored for the data grouped by "
                      "clusters, baskets/tissues or response.")
             st.write(" ")
-            option = st.selectbox("Select how to group samples", ('Clusters', 'Baskets/Tissues', 'Responsive'), key="PCA")
+            # Option to select level of grouping: clusters, baskets or response
+            option_PCA = st.selectbox("Select how to group samples", ('Clusters', 'Baskets/Tissues', 'Responsive'), key="PCA")
+            #Option to show data in table format
             RawD = st.checkbox("Show raw data", key="raw-data-PCA")
-            if option == "Clusters":
+            if option_PCA == "Clusters":
                 analysis.PCA_analysis("cluster_number", RawD)
-            elif option == "Responsive":
+            elif option_PCA == "Responsive":
                 analysis.PCA_analysis("responsive", RawD)
-            elif option == 'Baskets/Tissues':
+            elif option_PCA == 'Baskets/Tissues':
                 analysis.PCA_analysis("tissues", RawD)
+        #Prototypes samples subpage
         with tab22:
             st.subheader("Prototypes")
             st.write("The prototypical sample of each cluster, basket/tissue or pattern of response has been calculated using"
@@ -132,10 +147,11 @@ if "data" in st.session_state:
                     " that is the closest to the rest of samples in the same group. The dimension of the expression level of transcripts"
                      " has been reduced and plotted using Principal Component Analysis (PCA). ")
             st.write(" ")
-            option = st.selectbox("Select how to group samples", ('Clusters', 'Baskets/Tissues'), key="Prototypes")
+            #Option to select level of grouping: clusters, baskets or response
+            option_prototype = st.selectbox("Select how to group samples", ('Clusters', 'Baskets/Tissues'), key="Prototypes")
             prototype = Prototypes(data)
-            prototype.findPrototypes(option)
-
+            prototype.findPrototypes(option_prototype)
+        #Differential expression analysis subpage
         with tab23:
             st.subheader("Differential expression")
             st.write(" ")
@@ -147,6 +163,7 @@ if "data" in st.session_state:
             st.write(" ")
             col51, col52 = st.columns((2, 2))
             with col51:
+                # Option to select level of grouping: clusters, baskets or response
                 option = st.selectbox("Select how to group samples", ('Clusters', 'Baskets/Tissues', 'Responsive'), key="DEA")
                 if option == 'Clusters':
                     subgroups = data.clusters_names
@@ -157,14 +174,17 @@ if "data" in st.session_state:
                 else:
                     subgroups = ["Responsive","Non-responsive"]
                     feature = "responsive"
+                #Option to select groups to perform DEA
                 groups = st.multiselect(
                     'Please select up to 2 Groups to compare', subgroups, max_selections=2)
             if len(groups)<2:
                 st.write("")
             else:
                 with col51:
+                    #User input for corrected p-value threshold
                     pthresh = st.number_input('P-value threshold for significance (0.05 by default)', value = 0.05)
                     st.caption("Applied on corrected p-values.")
+                    #User input for log2 fold change threshold
                     logthresh = st.number_input('log2 FC threshold for significance (1 by default)', value=1.0)
                 dea = DEA(data)
                 dea.diffAnalysis_simple(groups[0],groups[1],feature,pthresh,logthresh)
@@ -177,6 +197,7 @@ if "data" in st.session_state:
                 st.write("The difference in the expression level of a selected transcript between groups being compared can be explored below. "
                          "Further information about the transcript can be found by using the GeneCards button. ")
                 st.write("")
+                #Option to select transcript results to be displayed
                 transcript = searchTranscripts(results["Feature"])
                 col53, col54 = st.columns((2,4))
                 with col53:
